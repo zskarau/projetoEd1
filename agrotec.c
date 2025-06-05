fe8bd85bb224417b0fc778407c956bd7750ca554
Lucas <lucaskarau@gmail.com>
2025-06-05 13:41:10 -0300

    adding txt and .c just adding txt files that are going to be used as inputs and creating .c file with the defined structs

---
+++ agrotec.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct produto{
+    int codigo;     
+    char nome[30];     
+    int quantidade;     
+    float precos[3];     
+    struct produto *prox;     
+}Produto;  
+ 
+typedef struct historico{    
+    int codigo;     
+    int vendas[4];     
+    struct historico *prox; 
+}HistoricoVendas;  
+
+typedef struct pedidoitem{     
+    int codigo_produto;    
+    int quantidade;     
+    struct pedidoitem *prox; 
+}PedidoItem; 
+
+typedef struct Pedido{ 
+    int id_pedido; 
+    PedidoItem *itens; 
+    struct Pedido *prox; 
+}Pedido; 
+
+typedef struct Cliente{ 
+    int id_cliente; 
+    char nome[30]; 
+    Pedido *pedidos; 
+    struct Cliente *prox; 
+}Cliente; 
+
+int main(){}
